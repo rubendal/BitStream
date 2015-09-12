3c592eb59ff3e9435d8b5c37527b4d560e6f9507
rubendal <rubendal@users.noreply.github.com>
2015-09-11 21:50:41 -0500

    Changed namespace

--- Bit.cs
+++ Bit.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace BitStreamLib
+namespace BitStreams
 {
     public class Bit
     {
--- BitStream.cs
+++ BitStream.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 
-namespace BitStreamLib
+namespace BitStreams
 {
     public class BitStream
     {
